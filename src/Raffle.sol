55c2a75a96a95523ecb19117e49f41ab8647289b
mabsattar <mabsattar@proton.me>
2026-08-19 02:27:50 +0530

    add functionality to filter and pay winner

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -18,6 +18,8 @@ contract Raffle is VRFConsumerBaseV2Plus {
 
     error Raffle__NotEnoughTime();
 
+    error Raffle__TransferFailed();
+
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
 
@@ -29,7 +31,8 @@ contract Raffle is VRFConsumerBaseV2Plus {
     uint32 private immutable i_callbackGasLimit;
 
     uint256 private s_lastTimeStamp;
-    address[] private s_players;
+    address payable[] private s_players;
+    address private s_recentWinner;
 
     /* Events */
     event RaffleEntered(address indexed players);
@@ -77,7 +80,15 @@ contract Raffle is VRFConsumerBaseV2Plus {
         uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
     }
 
-    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {}
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        (bool success,) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+    }
 
     /**
      * getter function
