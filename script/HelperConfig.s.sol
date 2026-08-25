b36115634c182270d6087ccf018d3fc253dad132
mabsattar <mabsattar@proton.me>
2026-08-26 03:47:41 +0530

    fix chainlink import and mock deployment

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {
     VRFCoordinatorV2_5Mock
-} from "@chainlink-brownie-contracts/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 
 abstract contract CodeConstants {
     uint256 public constant ETH_SEPOLIA_CHAIN_ID = 1115511;
@@ -41,7 +41,7 @@ contract HelperConfig is Script, CodeConstants {
         if (networkConfigs[chainId].vrfCoordinator != address(0)) {
             return networkConfigs[chainId];
         } else if (chainId == LOCAL_CHAIN_ID) {
-            // getOrCreateAnvilEthConfig()
+            return getOrCreateAnvilEthConfig();
         } else {
             revert HelperConfig__InvalidChainId();
         }
@@ -66,17 +66,18 @@ contract HelperConfig is Script, CodeConstants {
 
         // Deploy mocks
         vm.startBroadcast();
-        vrfCoordinatorV2_5Mock vrfCoordinatorMock =
-            new VRDCoordinatorV2_5Mock(MOCK_BASE_FEE, MOCK_GAS_PRICE_LINK, MOCK_WEI_PER_UINT_LINK);
+        VRFCoordinatorV2_5Mock vrfCoordinatorMock =
+            new VRFCoordinatorV2_5Mock(MOCK_BASE_FEE, MOCK_GAS_PRICE_LINK, MOCK_WEI_PER_UINT_LINK);
         vm.stopBroadcast();
 
         localNetworkConfig = NetworkConfig({
             entranceFee: 0.01 ether,
             interval: 30,
-            vrfCoordinator: address(vrfCoordinator),
+            vrfCoordinator: address(vrfCoordinatorMock),
             gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
             callbackGasLimit: 500000,
             subscriptionId: 0
         });
+        return localNetworkConfig;
     }
 }
