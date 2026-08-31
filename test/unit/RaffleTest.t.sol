a5e62f828c257db96a295dd22272e3ecaf7712cf
mabsattar <mabsattar@proton.me>
2026-08-31 21:36:11 +0530

    update setUp() and add some test functions

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -8,7 +8,55 @@ import {DeployRaffle} from "script/DeployRaffle.s.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
 
 contract RaffleTest is Test {
-    function setup() external {
+    Raffle public raffle;
+    HelperConfig public helperConfig;
+
+    uint256 entranceFee;
+    uint256 interval;
+    address vrfCoordinator;
+    bytes32 gasLane;
+    uint256 subscriptionId;
+    uint32 callbackGasLimit;
+
+    address public PLAYER = makeAddr("player");
+    uint256 public constant STARTING_PLAYER_BALANCE = 10 ether;
+
+    function setUp() external {
         DeployRaffle deployer = new DeployRaffle();
+        (raffle, helperConfig) = deployer.deployContract();
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+        entranceFee = config.entranceFee;
+        interval = config.interval;
+        vrfCoordinator = config.vrfCoordinator;
+        gasLane = config.gasLane;
+        subscriptionId = config.subscriptionId;
+        callbackGasLimit = config.callbackGasLimit;
+
+	vm.deal(PLAYER, STARTING_PLAYER_BALANCE);
+    }
+
+    function testRaffleInitializesInOpenState() public view {
+        assert(raffle.getRaffleState() == Raffle.RaffleState.OPEN);
+    }
+
+    //////////////////
+    // ENTER RAFFLE //
+    //////////////////
+    function testRaffleRevertWhenYouDontPayEnough() public {
+	// Arrange
+	vm.prank(PLAYER);
+	// Act 	// Asset
+        vm.expectRevert(Raffle.Raffle__NotEnoughEth.selector);
+	raffle.enterRaffle();
+    }
+
+    function testRaffleRecordsPlayersWhenTheyEnter() public {
+	// Arrange
+	vm.prank(PLAYER);
+	// Act
+	raffle.enterRaffle{value: entranceFee}();
+	// Assert
+	address playerRecorded = raffle.getPlayer(0);
+	assert(playerRecorded == PLAYER);
     }
 }
